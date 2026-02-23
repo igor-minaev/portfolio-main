84345b5195f16ee4ab05dbd7700c399bbc2fe2d6
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-23 18:28:30 +0300

    add Header component

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import {Menu} from "../../components/menu/Menu.tsx";
+import {Logo} from "../../components/logo/Logo.tsx";
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <Logo/>
+            <Menu/>
+        </StyledHeader>
+    );
+};
+
+const StyledHeader = styled.header`
+    background-color: #59f8bf;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
