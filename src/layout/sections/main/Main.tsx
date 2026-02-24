0731dd116f8d0be823ccd465083ab61416b1d555
Igor Minayeu <igor.minayeu@gmail.com>
2026-02-24 18:00:53 +0300

    add Main component

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,42 @@
+import image from '../../../assets/images/blogging.svg'
+import styled from "styled-components";
+import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
+
+export const Main = () => {
+    return (
+        <StyledMain>
+           <FlexWrapper justify="space-around" alignItems="center
+           ">
+               <TextContainer>
+                   <SmallText>DEVELOPER</SmallText>
+                   <Name>Erica Font</Name>
+                   <Description>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+                       consequat
+                       duis enim velit mollit. Exercitation veniam consequat sunt.</Description>
+                   <a href="">Contact Me</a>
+               </TextContainer>
+               <Photo src={image} alt="blogging"/>
+           </FlexWrapper>
+        </StyledMain>
+    );
+};
+
+const StyledMain = styled.div`
+    background-color: #bfe0f4;
+    height: 100vh;
+    position: relative;
+    
+`
+const TextContainer = styled.div`
+    max-width: 514px;
+`
+const SmallText = styled.span`
+`
+const Name = styled.h2`
+`
+const Description = styled.p`
+`
+
+const Photo = styled.img`
+
+`
