70eb894efcf6170ffb3c792b1b6349a342fc15b7
Igor Minayeu <igor.minayeu@gmail.com>
2026-03-01 11:57:35 +0300

    add theme for styles

---
+++ src/styles/Theme.ts
@@ -0,0 +1,9 @@
+export const theme={
+    colors:{
+        background:"#fafafa",
+        primaryFont: "#1f2626",
+        secondaryFont:"#151717",
+        tertiaryFont:"#dbffff",
+        accent:"#5222d0"
+    }
+}
